bd8f44949640c2f30f9c7d1c55c93c9dea45dfb1
Szymon Stefański <szymonstefanski1@gmail.com>
2025-02-22 21:57:36 +0100

    Fix currency display bug on website, add correct API response

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__, static_folder='static')
-
 
 @app.route("/")
 def home():
@@ -11,9 +11,15 @@ def home():
 def about():
     return render_template("about.html")
 
-@app.route("https://api.nbp.pl/api/exchangerates/rates/a/<currency>/")
-def about(currency):
-    return {"currency": currency}
+@app.route("/api/exchangerates/rates/a/<currency>/")
+def value(currency):
+    url = f"https://api.nbp.pl/api/exchangerates/rates/a/{currency}/"
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        return {"error": "Invalid request"}, response.status_code
+
+    return response.json()
 
 if __name__ == "__main__":
     app.run(debug=True)
