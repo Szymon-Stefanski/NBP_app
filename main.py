c560cf5bac862f9a62c9922a3d1b03e1f98094b2
Szymon Stefański <szymonstefanski1@gmail.com>
2025-02-22 22:38:15 +0100

    Add API to retrieve data about gold prices from NBP

--- main.py
+++ main.py
@@ -21,5 +21,48 @@ def value(currency):
 
     return response.json()
 
+
+# GOLD PRICES:
+@app.route("/api/cenyzlota")
+def gold():
+    url = f"https://api.nbp.pl/api/cenyzlota/"
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        return {"error": "Invalid request"}, response.status_code
+
+    return response.json()
+
+@app.route("/api/cenyzlota/last/<topCount>")
+def gold_top(topCount):
+    url = f"https://api.nbp.pl/api/cenyzlota/last/{topCount}"
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        return {"error": "Invalid request"}, response.status_code
+
+    return response.json()
+
+@app.route("/api/cenyzlota/<date>")
+def gold_date(date):
+    url = f"https://api.nbp.pl/api/cenyzlota/{date}"
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        return {"error": "Invalid request"}, response.status_code
+
+    return response.json()
+
+@app.route("/api/cenyzlota/<startDate>/<endDate>")
+def gold_from_to(startDate, endDate):
+    url = f"https://api.nbp.pl/api/cenyzlota/{startDate}/{endDate}"
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        return {"error": "Invalid request"}, response.status_code
+
+    return response.json()
+
+
 if __name__ == "__main__":
     app.run(debug=True)
