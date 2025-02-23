6dc129a3e8b05fb6c4664aab0c05ed7d7c8330db
Szymon Stefański <szymonstefanski1@gmail.com>
2025-02-23 01:37:17 +0100

    Add API to retrieve data about currencies from NBP

--- main.py
+++ main.py
@@ -11,9 +11,40 @@ def home():
 def about():
     return render_template("about.html")
 
+# CURRENCIES - EXCHANGE RATES:
 @app.route("/api/exchangerates/rates/a/<currency>/")
 def value(currency):
     url = f"https://api.nbp.pl/api/exchangerates/rates/a/{currency}/"
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        return {"error": "Invalid request"}, response.status_code
+
+    return response.json()
+
+@app.route("/api/exchangerates/rates/a/<currency>/<date>")
+def value_date(currency, date):
+    url = f"https://api.nbp.pl/api/exchangerates/rates/a/{currency}/{date}"
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        return {"error": "Invalid request"}, response.status_code
+
+    return response.json()
+
+@app.route("/api/exchangerates/rates/a/<currency>/<startDate>/<endDate>")
+def value_from_to(currency, startDate, endDate):
+    url = f"https://api.nbp.pl/api/exchangerates/rates/a/{currency}/{startDate}/{endDate}"
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        return {"error": "Invalid request"}, response.status_code
+
+    return response.json()
+
+@app.route("/api/exchangerates/rates/a/<currency>/last/{topCount}")
+def value_top(currency, topCount):
+    url = f"https://api.nbp.pl/api/exchangerates/rates/a/{currency}/{topCount}"
     response = requests.get(url)
 
     if response.status_code != 200:
