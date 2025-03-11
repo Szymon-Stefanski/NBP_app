8ee080aaf3cccd7c75fad2037735bdec008d97de
Szymon Stefański <szymonstefanski1@gmail.com>
2025-03-11 22:21:39 +0100

    Add feature to generate currencies course charts from last 30 days in Charts section

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, timedelta
 import plotly.express as px
 import pandas as pd
 import requests
@@ -7,6 +8,7 @@ import requests
 app = Flask(__name__, static_folder='static')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///nbp.db'
 db = SQLAlchemy(app)
+
 
 class Currencies(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -90,20 +92,6 @@ def display_gold():
     return render_template("gold.html", gold=gold)
 
 
-@app.route("/charts/")
-def charts():
-    data = {
-        "date": ["2025-02-01", "2025-02-17", "2025-03-03"],
-        "value": [4.20, 4.25, 4.18]
-    }
-    df = pd.DataFrame(data)
-
-    fig = px.line(df, x="date", y="value", title="Kurs EUR/PLN")
-    graph_html = fig.to_html(full_html=False)
-
-    return render_template("charts.html", graph_html=graph_html)
-
-
 @app.route("/calculator/", methods=['GET', 'POST'])
 def calculator():
     currencies = Currencies.query.all()
@@ -123,94 +111,42 @@ def calculator():
     return render_template("calculator.html", currencies=currencies, result=result)
 
 
-# CURRENCIES - EXCHANGE RATES:
-@app.route("/api/exchangerates/rates/a/<currency>/")
-def value(currency):
-    url = f"https://api.nbp.pl/api/exchangerates/rates/a/{currency}/"
-    response = requests.get(url)
+@app.route("/charts/", methods=['GET', 'POST'])
+def charts():
+    currencies = Currencies.query.all()
 
-    if response.status_code != 200:
-        return {"error": "Invalid request"}, response.status_code
+    today = datetime.today()
+    thirty_days_ago = today - timedelta(days=30)
+    formatted_date = thirty_days_ago.strftime('%Y-%m-%d')
 
-    return response.json()
+    data = {
+        "date": [],
+        "value": []
+    }
 
+    graph_html = None
 
-@app.route("/api/exchangerates/rates/a/<currency>/<date>")
-def value_date(currency, date):
-    url = f"https://api.nbp.pl/api/exchangerates/rates/a/{currency}/{date}"
-    response = requests.get(url)
+    if request.method == 'POST':
+        code = request.form["code"]
 
-    if response.status_code != 200:
-        return {"error": "Invalid request"}, response.status_code
+        url = (f"https://api.nbp.pl/api/exchangerates/rates/a/{code}/{formatted_date}/"
+               f"{today.strftime('%Y-%m-%d')}/")
+        get = requests.get(url)
+        content = get.json()
 
-    return response.json()
+        if "rates" in content and content["rates"]:
+            for rate in content["rates"]:
+                data["date"].append(rate["effectiveDate"])
+                data["value"].append(rate["mid"])
 
+            df = pd.DataFrame(data)
 
-@app.route("/api/exchangerates/rates/a/<currency>/<startDate>/<endDate>")
-def value_from_to(currency, startDate, endDate):
-    url = f"https://api.nbp.pl/api/exchangerates/rates/a/{currency}/{startDate}/{endDate}"
-    response = requests.get(url)
+            fig = px.line(df, x="date", y="value", title=f"Course {code}/PLN")
+            graph_html = fig.to_html(full_html=False)
+        else:
+            print("API problem.")
 
-    if response.status_code != 200:
-        return {"error": "Invalid request"}, response.status_code
-
-    return response.json()
-
-
-@app.route("/api/exchangerates/rates/a/<currency>/last/{topCount}")
-def value_top(currency, topCount):
-    url = f"https://api.nbp.pl/api/exchangerates/rates/a/{currency}/{topCount}"
-    response = requests.get(url)
-
-    if response.status_code != 200:
-        return {"error": "Invalid request"}, response.status_code
-
-    return response.json()
-
-
-# GOLD PRICES:
-@app.route("/api/cenyzlota")
-def gold():
-    url = f"https://api.nbp.pl/api/cenyzlota/"
-    response = requests.get(url)
-
-    if response.status_code != 200:
-        return {"error": "Invalid request"}, response.status_code
-
-    return response.json()
-
-
-@app.route("/api/cenyzlota/last/<topCount>")
-def gold_top(topCount):
-    url = f"https://api.nbp.pl/api/cenyzlota/last/{topCount}"
-    response = requests.get(url)
-
-    if response.status_code != 200:
-        return {"error": "Invalid request"}, response.status_code
-
-    return response.json()
-
-
-@app.route("/api/cenyzlota/<date>")
-def gold_date(date):
-    url = f"https://api.nbp.pl/api/cenyzlota/{date}"
-    response = requests.get(url)
-
-    if response.status_code != 200:
-        return {"error": "Invalid request"}, response.status_code
-
-    return response.json()
-
-
-@app.route("/api/cenyzlota/<startDate>/<endDate>")
-def gold_from_to(startDate, endDate):
-    url = f"https://api.nbp.pl/api/cenyzlota/{startDate}/{endDate}"
-    response = requests.get(url)
-
-    if response.status_code != 200:
-        return {"error": "Invalid request"}, response.status_code
-
-    return response.json()
+    return render_template("charts.html", graph_html=graph_html, currencies=currencies)
 
 
 if __name__ == "__main__":
